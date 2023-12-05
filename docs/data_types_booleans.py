c64cbde560a641b2939d6ad9ffc6596e4439b7a9
ohjisu320 <ohjisu320@gmail.com>
2023-12-05 16:02:21 +0900

    commit

--- docs/data_types_booleans.py
+++ docs/data_types_booleans.py
@@ -14,3 +14,31 @@ str_name=="ohjisu"
 # 같지 않음에 대한 기호 '!='
 str_name!="ohjisu"
 # True
+
+# 숫자에 대한 판단 (변수+부등호+기준값)
+# 컴퓨터 입장
+# True = 1, False = 0
+
+5==4
+# False
+num_first=5
+num_first == 4
+# False
+num_first != 4
+# True
+num_first > 4  # 초과
+# True
+num_first >= 4 # 이상
+# True
+pass
+
+# if - elif - else
+# 점수에 따른 표시
+# 90점 이상: A, 80점 초과: B, 나머지 F
+pass
+my_score=90
+my_score>=90
+#True
+my_score=80
+my_score>80
+# False
--- docs/if_elses.py
+++ docs/if_elses.py
@@ -1,10 +1,10 @@
 # 기본 if elif else 구문
-if True : 
-    pass
-elif True :
-    pass
-else :
-    pass
+# if True : 
+#     pass
+# elif True :
+#     pass
+# else :
+#     pass
 
 #문자 비교
 str_name="oh jisu"
@@ -16,11 +16,52 @@ if str_name=="oh jisu" :
     print("name is {}.".format(str_name))
 print("End Program!")
 
-# if문의 질문 형식(condition): 변수+부등호+기준값
+# if문의 질문 형식(condition=조건절): 변수+부등호+기준값
 #문자에 대한 부정으로 질문
 if str_name!="oh jisu" :  
     pass
     print("name is not {}.".format(str_name))
 print("End Program!")
 
-# 숫자에 대한 판단
+# if - else
+# num_first >= 4 -> True : "큼", False : "작음"
+num_first = 5
+if num_first >= 4 : 
+    pass    # condition이 True
+    print("{}는 4보다 크다".format(num_first))
+else :
+    pass    # condition이 False
+    print("{}는 4보다 작다".format(num_first))
+print("End Program!")
+pass
+
+num_first = 5
+if num_first >= 6 : 
+    pass    # condition이 True
+    print("{}는 6보다 크다".format(num_first))
+else :
+    pass    # condition이 False
+    print("{}는 6보다 작다".format(num_first))
+print("End Program!")
+pass
+
+# if - elif - else
+# 점수에 따른 표시
+# 90점 이상: A, 80점 초과: B, 나머지 F
+pass
+# my_score = 90
+
+
+my_score = 79
+
+if my_score >= 90 : # 90점 이상: A
+    pass
+    print("{}점은 90점 이상으로 A 학점.".format(my_score))
+elif my_score > 80 : # 80점 초과: B
+    pass
+    print("{}점은 80점 초과이므로 B 학점.".format(my_score))
+else :               # 나머지 F
+    pass
+    print("{}점은 80점 이하이므로 F 학점.".format(my_score))
+    pass
+print("End Program!")
