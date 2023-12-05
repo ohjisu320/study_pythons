be042db969a1e04cccad0d9d2610ccf7315af141
ohjisu320 <ohjisu320@gmail.com>
2023-12-05 17:42:23 +0900

    modify

---
+++ docs/quests/timestables.py
@@ -0,0 +1,19 @@
+#5단
+num_vir=0
+while num_vir<45 :
+    pass
+    num_vir=num_vir+5
+    print("{}".format(num_vir))
+    pass
+print("End Program")
+
+#option: input 받아서
+num_vir=input("구구단 몇단? : ")
+num_vir=int(num_vir)
+num_vir_sec=0
+while num_vir_sec<num_vir*9 :
+    pass
+    num_vir_sec=num_vir_sec+num_vir
+    print("{}".format(num_vir_sec))
+    pass
+print("End Program")
