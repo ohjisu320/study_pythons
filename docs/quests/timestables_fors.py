9e895aac2c1844d2a283781bc8da5f9ca51a3636
ohjisu320 <ohjisu320@gmail.com>
2023-12-06 11:42:28 +0900

    modify

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -1,8 +1,8 @@
-numbering=[1, 2, 3, 5, 6, 7, 8, 9]
-str_input=input("구구단 몇단 : ")
-num_input=int(str_input)
+numbering=[0, 1, 2, 3, 5, 6, 7, 8]
+num_input=int(input("구구단 몇단 : "))
+
 
 for loop in numbering:
     pass
-    print("{}".format(loop*input))
+    print("{}*{}={}".format(num_input, loop+1, (loop+1)*num_input))
 print("End Program")
