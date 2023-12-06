8d99855f77cfcd7210c43155fa91820538cf1b29
ohjisu320 <ohjisu320@gmail.com>
2023-12-06 16:20:18 +0900

    commit

--- docs/outputs.py
+++ docs/outputs.py
@@ -14,3 +14,11 @@ if 5>2: # 묶음기호인 :과 tab은 하나의 쌍
     pass #디버깅 할 때 유용
     print("Five is greater than two!")
 print("end")
+
+# 한 줄에 출력
+first = "First"
+second = "Second"
+print("first : {}!".format(first), end=", 다음 줄 ")
+print("second : {}!".format(second))
+print("{} {}".format(first, second))
+print("End Program!")
