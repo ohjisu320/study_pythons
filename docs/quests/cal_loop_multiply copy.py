652e150daaa04c584bc34a336b05ba4010c28d7d
ohjisu320 <ohjisu320@gmail.com>
2023-12-07 17:50:04 +0900

    commit

--- docs/quests/cal_loop_multiply copy.py
+++ docs/quests/cal_loop_multiply copy.py
@@ -2,15 +2,8 @@
 def multiply(num_first, num_second): # 입력하는 값
     multiply_result=num_first*num_second
     return multiply_result # 나오는 값
-def q_sign_result():
-    q_program ="End Program!"
-    return q_program
-
-cal_number=(input("multiply-first multiply-second : ")).split()
-# 곱셈 값 출력
-cal_multiply = multiply(int(cal_number[0]), int(cal_number[1]))
-print_multiply_result=print("multiply result : {}".format(cal_multiply))
-# 종료될 때 까지 반복
+# 출력기
+def printer(multiply)
 
 # while True :
 #     if q_sign_input!="q" :
