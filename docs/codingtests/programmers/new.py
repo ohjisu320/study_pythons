9020e6420b505da74a1c23bcf9c8ede7cd822a90
ohjisu320 <ohjisu320@gmail.com>
2024-03-25 17:50:16 +0900

    new

---
+++ docs/codingtests/programmers/new.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/258712
+# 친구들의 이름을 담은 1차원 문자열 배열 friends 이번 달까지 친구들이 주고받은 선물 기록을 담은 1차원 문자열 배열 gifts가 매개변수로 주어집니다.
+# 이때, 다음달에 가장 많은 선물을 받는 친구가 받을 선물의 수를 return 하도록 solution 함수를 완성해 주세요.
+
+def solution(friends, gifts):
+    answer = 0
+    return answer
+
+friends = list(input().split(","))
+gifts = list(input().split(","))
+pass
