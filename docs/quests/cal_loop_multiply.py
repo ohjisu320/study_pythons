9f97a5c80ef6e6d2018b483bb72751445e8a2905
ohjisu320 <ohjisu320@gmail.com>
2024-04-05 12:46:06 +0900

    push

--- docs/quests/cal_loop_multiply.py
+++ docs/quests/cal_loop_multiply.py
@@ -1,28 +1,42 @@
 # 곱셈 계산기
-def multiply(num_first, num_second): # 입력하는 값
-    multiply_result=num_first*num_second
-    return multiply_result # 나오는 값
-
-# 곱셈 값 입력
-cal_first=int(input("multiply first : "))
-cal_second=int(input("multiply second : "))
- # 곱셈 값 출력
-cal_multiply = multiply(cal_first, cal_second)
-print_multiply_result=print("multiply result : {}".format(cal_multiply))
-# 종료 사인 입력
-q_sign_input=input("종료하려면 q를 입력하세요 : ")
-
-# 종료될 때 까지 반복
-while q_sign_input!='q' :
-    # 곱셈 값 입력
-    cal_first=int(input("multiply first : "))
-    cal_second=int(input("multiply second : "))
-    # 곱셈 값 출력1
-    cal_multiply = multiply(cal_first, cal_second)
+def printer():
+    cal_number=int(input("multifly first : ")), int(input("multifly second : "))
+    cal_multiply = cal_number[0]*cal_number[1]
     print_multiply_result=print("multiply result : {}".format(cal_multiply))
-    q_sign_input=input("종료하려면 q를 입력하세요 : ")
-else:
+    end=input("종료하려면 q를 입력해주세요 : ")
+    return end
+
+# end_mastrer작동되지 않음.
+def end_master(endsign) :
+    while endsign !="q" :
+        printer()
     print("End Program!")
+
+
+endsign=printer()
+end_master(endsign)
+
+
+# # 곱셈 계산기
+# def multiply(num_first, num_second): # 입력하는 값
+#     multiply_result=num_first*num_second
+#     return multiply_result # 나오는 값
+# # 출력기
+
+
+
+# multiply()
+
+# while True :
+#     if q_sign_input!="q" :
+#         # 곱셈 값 입력
+#         cal_number=int(input("multifly first : ")), int(input("multifly second : "))
+#         # 곱셈 값 출력
+#         cal_multiply = multiply(cal_number[0], cal_number[1])
+#         print_multiply_result=print("multiply result : {}".format(cal_multiply))
+#     else :
+#         break
+
 
 
 
